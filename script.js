26f65ce0718ea0020c2803c175ac384d401105ac
Andrew <abysmalduck@gmail.com>
2022-06-15 16:24:12 -0700

    Style for mobile-first, logic works well

--- script.js
+++ script.js
@@ -20,47 +20,44 @@ async function getWeatherData(city) {
 
 async function processWeatherData(data) {
   return {
-    temp: data.main.temp,
-    temp_min: data.main.temp_min,
-    temp_max: data.main.temp_max,
+    temp: Math.round(data.main.temp),
+    temp_min: Math.round(data.main.temp_min),
+    temp_max: Math.round(data.main.temp_max),
     humidity: data.main.humidity,
-    feels_like: data.main.feels_like,
+    feels_like: Math.round(data.main.feels_like),
     city: data.name,
     weather: {
       description: data.weather[0].description,
       icon: data.weather[0].icon,
+      id: data.weather[0].id,
     },
   };
 }
 
 function refreshWeather(data) {
-  const parent = document.querySelector(".weather");
-  const city = document.createElement("h1");
-  city.textContent = data.city;
-  const icon = document.createElement("img");
+  //   const parent = document.querySelector(".cw-weather");
+  const city = document.querySelector(".cw-city");
+  city.textContent = `${data.city}`;
+  const icon = document.querySelector(".cw-icon");
   icon.src = `http://openweathermap.org/img/w/${data.weather.icon}.png`;
-  const weather = document.createElement("h2");
-  weather.textContent = `${data.weather.description}`;
-  const temp = document.createElement("p");
+  const weather = document.querySelector(".cw-weather-desc");
+  weather.textContent = `${data.weather.description
+    .trim()
+    .toLowerCase()
+    .replace(/\w\S*/g, (w) => w.replace(/^\w/, (c) => c.toUpperCase()))}`;
+  const temp = document.querySelector(".cw-temp");
   temp.textContent = `Current Temp: ${data.temp}°`;
-  const mintemp = document.createElement("p");
+  const mintemp = document.querySelector(".cw-mintemp");
   mintemp.textContent = `Low: ${data.temp_min}°`;
-  const maxtemp = document.createElement("p");
+  const maxtemp = document.querySelector(".cw-maxtemp");
   maxtemp.textContent = `High: ${data.temp_max}°`;
-  const humidity = document.createElement("p");
-  humidity.textContent = `Humidity: ${data.humidity}`;
-  const feelslike = document.createElement("p");
+  const humidity = document.querySelector(".cw-humidity");
+  humidity.textContent = `Humidity: ${data.humidity}%`;
+  const feelslike = document.querySelector(".cw-feelslike");
   feelslike.textContent = `Feels like: ${data.feels_like}°`;
-  parent.append(
-    city,
-    icon,
-    weather,
-    temp,
-    mintemp,
-    maxtemp,
-    humidity,
-    feelslike
-  );
 }
 
 btn.addEventListener("click", () => getWeatherData(cityInput.value));
+
+// Load default weather data
+getWeatherData("San Diego");
